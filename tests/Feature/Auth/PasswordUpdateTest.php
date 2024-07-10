048af4da3d2f53e29ca976f294e6cafd6a38770f
RazinTeqB <razin@teqbuddies.com>
2024-07-10 10:46:33 +0530

    laravel breeze added, ide helper, debug bar, pint added

---
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Hash;
+
+test('password can be updated', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $component = Volt::test('profile.update-password-form')
+        ->set('current_password', 'password')
+        ->set('password', 'new-password')
+        ->set('password_confirmation', 'new-password')
+        ->call('updatePassword');
+
+    $component
+        ->assertHasNoErrors()
+        ->assertNoRedirect();
+
+    $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+});
+
+test('correct password must be provided to update password', function () {
+    $user = User::factory()->create();
+
+    $this->actingAs($user);
+
+    $component = Volt::test('profile.update-password-form')
+        ->set('current_password', 'wrong-password')
+        ->set('password', 'new-password')
+        ->set('password_confirmation', 'new-password')
+        ->call('updatePassword');
+
+    $component
+        ->assertHasErrors(['current_password'])
+        ->assertNoRedirect();
+});
